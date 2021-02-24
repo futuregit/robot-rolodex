bc56b503fe3e640c3080c9c7476f3bc91ac8449c
futuregit <tomcis102@hotmail.com>
2021-02-24 00:01:58 -0500

    Added components folder with sub folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
+
 import './App.css';
 
 class App extends Component {
@@ -19,6 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name='Tman'/>
        {
          this.state.robots.map( robot => 
             <h1 key={robot.id} > {robot.name} </h1> 
