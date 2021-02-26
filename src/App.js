300e72fedb6bb7dac3ab3a78bca574efe0522236
futuregit <tomcis102@hotmail.com>
2021-02-25 23:58:32 -0500

    Added searchbox component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 
 import { CardList } from './components/card-list/card-list.component';
 
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -28,10 +30,10 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input 
-          type='search' 
-          placeholder='search robots' 
-          onChange={e => this.setState({searchField: e.target.value})}  />
+        <SearchBox
+          placeholder='search robots'
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
         <CardList robots={filterRobots} />       
       </div>
     );
